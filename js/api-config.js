86a320b6b39abf410965f1cf97e8d8ab179a2f6f
Rafa-Shauny <0000112159833xsp@al.educacao.sp.gov.br>
2025-03-27 19:47:44 -0300

    Update api-config.js

--- js/api-config.js
+++ js/api-config.js
@@ -1,1 +1,17 @@
-const OXR_API_KEY = '1ef3961459d64ad88ccdd58f154ef959'; 
+const API_CONFIG = {
+    OPEN_EXCHANGE_RATES: {
+        KEY: '1ef3961459d64ad88ccdd58f154ef959',
+        BASE_URL: 'https://openexchangerates.org/api'
+    },
+    FORMSPREE: {
+        FORM_ID: 'xblggpdw'
+    }
+};
+
+const FALLBACK_RATES = {
+    "BRL": 5.20,
+    "USD": 1,
+    "EUR": 0.85,
+    "RUB": 75.50,
+    "timestamp": Date.now()
+};
